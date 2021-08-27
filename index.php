dd2a0c7decfd6d5b00c4fcad4d673656808fc6c5
gislene84 <50671914+gislene84@users.noreply.github.com>
2021-08-27 16:51:46 -0300

    Manipular elementos

--- index.php
+++ index.php
@@ -25,6 +25,14 @@
                 <li><a href="">SOBRE</a></li>
                 <li><a href="">CONTATO</a></li>
             </ul><!--menu-desktop-->
+            <div class="menu-mobile">
+                <i class="fas fa-align-right"></i>
+                <ul>
+                    <li><a href="">HOME</a></li>
+                    <li><a href="">SOBRE</a></li>
+                    <li><a href="">CONTATO</a></li>
+                </ul>
+            </div><!--menu-mobile-->
             <!--clear: evitar que tenha conflitos no fluxo do site-->
                 <div class="clear"></div><!--clear-->
                 <br/>
@@ -41,30 +49,119 @@
                 <div class="clear"></div><!--clear-->
         </div><!--center-->
     </section><!--topo-->
+    <div class="circle"><i class="fas fa-angle-down"></i></div><!--circle-->
     <section class="clientes-slider">
-        <div class="circle"><i class="fas fa-angle-down"></i></div><!--circle-->
         <div class="center">
-
+            <div style="max-width: 700px; margin: 0 auto; overflow: hidden; " class="slider-container">
+            <img src="img/amazon.png"/>
+            <img src="img/costco.png" />
+            <img src="img/dominos.png" />
+            <img src="img/uber.png" />
+            <img src="img/walmart.png"/>
+            </div><!--slider-container-->
         </div><!--center-->
 
     </section><!--clientes-slider-->
+    <section class="diferenciais">
+        <div class="center">
+            <h2>Contribuímos de ponta a ponta</h2>
+            <div class="icons-diferenciais">
+                <div class="box-single-diferenciais">
+                    <img src="img/icon01.png">
+                    <h3>Ambientes Mobile</h3>
+                    <p>Garanta que toda a sua comunicação esteja alinhado com seu propósito, cada palavra conta.</p>
+                </div><!--box-single-diferenciais-->
+                <div class="box-single-diferenciais">
+                    <img src="img/icon2.png">
+                    <h3>Ambientes Mobile</h3>
+                    <p>Garanta que toda a sua comunicação esteja alinhado com seu propósito, cada palavra conta.</p>
+                </div><!--box-single-diferenciais-->
+                <div class="box-single-diferenciais">
+                    <img src="img/icon3.png">
+                    <h3>Ambientes Mobile</h3>
+                    <p>Garanta que toda a sua comunicação esteja alinhado com seu propósito, cada palavra conta.</p>
+                </div><!--box-single-diferenciais-->
+            </div><!--icons-diferenciais-->
+        </div><!--center-->
+    </section><!--diferenciais-->
+    <section class="sobre-time">
+        <div class="center">
+            <div class="w50 time-descricao-2">
+                <h2>Um time experiente, <br/>comunicador e coeso.</h2>
+                <p>A Product Runt acredita que marcas fortes são contruídas a partir de transformações importantes e positivas na vida dos consumidores. Exatamente como uma conversa que ganha forma, a ideia vira flâmula.
+                <br/> 
+                <br/>
+                Comunicar bem, de forma coerente e clara não é algo fácil e estamos aqui para ajudar você e o seu time. Você escolhe, remoto ou presencial, para seu cliente ou seu público interno, você determina como vamos te ajudar.</p>
+            </div><!--w50 time-descricao-2-->
+            <div class="w50 img-time">
+                <img src="img/ilustracao-04.png"/>
+            </div><!--w50 img-time-->
+            <div class="clear"></div>
+        </div><!--center-->
+    </section><!--sobre-time-->
+    <section class="depoimentos">
+        <div class="center">
+            <h2>Depoimentos</h2>
+            <div class="depoimentos-box">
+                <div class="depoimento-single">
+                    <p>" Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum non vero necessitatibus quis voluptatem libero dicta, illo veniam minus quaerat. "</p>
+                    <p>Gislene Oliveira</p>
+                    <img src="img/oval-2.png"/>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p>" Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum non vero necessitatibus quis voluptatem libero dicta, illo veniam minus quaerat. "</p>
+                    <p>Gislene Oliveira</p>
+                    <img src="img/oval-2.png"/>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p>" Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum non vero necessitatibus quis voluptatem libero dicta, illo veniam minus quaerat. "</p>
+                    <p>Gislene Oliveira</p>
+                    <img src="img/oval-2.png"/>
+                </div><!--depoimento-single-->
+            </div><!--depoimentos-box-->
+        </div><!--center-->
+    </section><!--depoimentos-->
+    <footer><!--rodapé-->
+    <section class="metodologia">
+        <div class="center">
+            <h2>Conheça nossa metodologia</h2>
+            <p>O que acha de fazermos o que mais gostamos de fazer? Conversar! <br/> Entre em contato por e-mail ou telefone.</p>
+            <a href="/contato">Entrar com o contato</a>
+        </div><!--center-->
+    </section><!--metodologia-->
+         <div class="center">
+
+         </div><!--center-->  
+    </footer>
 
     <script src="js/jquery.js"></script>
     <script src="js/slick.min.js"></script>
     <script type="text/javascript">
 
-    $('.container-slider').slick({ 
+    $('section.clientes-slider .slider-container').slick({ 
         dots: true,
         arrows:false,
-        infinite: true,
-        centerMode: true,
-        centerPadding:0,
+        infinite: false,
         speed:1000,
-        slidesToShow: 1,
+        slidesToShow: 4,
         autoplay: true,
         autoplaySpeed: 3000,
         pauseOnHover:false
         });
+        $('section.depoimentos .depoimentos-box').slick({ 
+        dots: true,
+        arrows:false,
+        infinite: true,
+        speed:1000,
+        slidesToShow: 1,
+        centerMode: false
+        });
+</script>
+<script>
+    //Menu responsivo// 
+    $('.menu-mobile i').click(function(){//ao clicar o menu vai aparecer //
+        $('.menu-mobile').find('ul').slideToggle();
+    })
 </script>
 </body>
 </html>
